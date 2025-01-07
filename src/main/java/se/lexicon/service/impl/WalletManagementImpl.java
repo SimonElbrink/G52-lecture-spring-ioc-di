fd5afbe17cad6c5cfd465a9e669e5fd1a5954a3b
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-01-07 10:32:27 +0100

    Crypto Currency App - managing - Wallet

---
+++ src/main/java/se/lexicon/service/impl/WalletManagementImpl.java
@@ -0,0 +1,37 @@
+package se.lexicon.service.impl;
+
+import se.lexicon.dao.WalletDao;
+import se.lexicon.exception.WalletNotFoundException;
+import se.lexicon.exception.WalletValidationException;
+import se.lexicon.model.Wallet;
+import se.lexicon.service.WalletManagement;
+
+import java.util.Optional;
+
+//Service
+// Where business logic happens, Connect to other services, Auth, Validation ...
+public class WalletManagementImpl implements WalletManagement {
+
+    private WalletDao walletDao;
+
+    public WalletManagementImpl(WalletDao walletDao) {
+        this.walletDao = walletDao;
+    }
+
+    @Override
+    public Wallet create(String walletName) {
+        // Logics, validation, Connect to other services.
+        if (walletName == null) throw new WalletValidationException("Param is not valid", "Wallet Name");
+        Wallet wallet = new Wallet(walletName);
+
+        return walletDao.createWallet(wallet);
+    }
+
+    @Override
+    public Wallet getById(String id) {
+        Optional<Wallet> wallet = walletDao.findWallet(id);
+
+        if(wallet.isEmpty()) throw new WalletNotFoundException("Wallet not found.");
+        return wallet.get();
+    }
+}
